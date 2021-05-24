cf44006a1519422d68247285c9139ccc3b22bd54
Andrew Gerst <gerst20051@gmail.com>
2021-05-23 18:28:14 -0700

    add httpartyapp default files generated by bundle gem

---
+++ scripts/httpartyapp/lib/httpartyapp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "httpartyapp/version"
+
+module Httpartyapp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ scripts/httpartyapp/lib/httpartyapp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Httpartyapp
+  VERSION = "0.1.0"
+end
